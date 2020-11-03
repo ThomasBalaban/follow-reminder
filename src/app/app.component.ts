a49fa7e7e645096a86ca3078c1f2fcedbe73eeba
Thomas Balaban <balabaninteractive@gmail.com>
2020-11-03 14:41:39 -0500

    working theory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'follow-reminder';
+export class AppComponent implements OnInit {
+  reminder: String
+  turnOn = false
+
+  constructor(
+    private cd: ChangeDetectorRef
+  ){}
+
+  ngOnInit() {
+    this.reminder = 'sfdg11'
+    var min = 1, max = 3;
+    var rand = 1
+
+    setInterval( () => {
+      this.reminder = this.reminder + '1'
+
+      var rand = Math.floor(Math.random() * (max - min + 1) + min);
+
+      console.log('Wait for ' + rand + ' minutes');
+      console.log(this.reminder);
+
+      this.turnOn = true
+
+      // setTimeout( () => {
+      //   this.turnOn = false
+      // }, 3000)
+
+    }, rand * 10000)
+  }
 }
